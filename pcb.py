b58e3dc8aba00f4fb33158d023cadcbf8f682698
adrianmoagne <adrianmoagne26@gmail.com>
2021-12-06 16:48:21 -0300

    o

--- pcb.py
+++ pcb.py
@@ -16,13 +16,13 @@ nomes = [
 ]
 class PCB:
     def __init__(self,pid,prior,endereco) -> None:
-        self.pid = pid
-        self.name = nomes[random.randint(0,len(nomes)-1)]
-        self.prior = prior
-        self.data_hora = datetime.now()
-        self.endereco_incial = endereco
-        self.endereco_final = None
-        self.tempo_execucao = random.randint(1,15) 
+        self.pid = pid #processo id
+        self.name = nomes[random.randint(0,len(nomes)-1)] #nome do processo
+        self.prior = prior #prioridade
+        self.data_hora = datetime.now() #hora de criação
+        self.endereco_incial = endereco 
+        self.tempo_execucao = random.randint(1,15) #tempo necessário para o processo executar
+        self.endereco_final = self.endereco_incial
         
    
 
@@ -33,6 +33,5 @@ class PCB:
             self.tempo_execucao -= 1
             
             
-
     def __str__(self) -> str:
         return f'[{self.pid}]' + '\t' + str(self.prior) +'\t' + str(self.data_hora) + '\t' + str(self.name)
--- queue.py
+++ queue.py
@@ -2,22 +2,24 @@ from pcb import PCB
 import random
 class Queue:
     def __init__(self,queue_id) -> None:
-        self.queue_id = queue_id
-        self.prior = random.randint(0,5) 
+        self.queue_id = queue_id #id da fila
+        self.prior = random.randint(0,5) #prioridade da fila
         self.quantum = random.randint(1,8) 
-        self.processos = []
+        self.processos = [] 
         self.atual_processo = 0
 
+    
     def add_processo(self, novo_processo: PCB):
         self.processos.append(novo_processo)
     
 
     def executar(self):
         self.processos[self.atual_processo].executar(self.quantum)
-
+        self.processos[self.atual_processo].endereco_final += 1 
         if self.processos[self.atual_processo].tempo_execucao == 0:
+            
             self.processos.pop(self.atual_processo)
-            self.atual_processo = 0 if (len(self.processos) == 0) else (self.atual_processo % (len(self.processos)))
+            self.atual_processo = 0 if (len(self.processos) == 0) else (self.atual_processo % (len(self.processos))) 
             return 
         self.atual_processo = (self.atual_processo + 1) % (len(self.processos))
     
